e9ae845a8c4b5b921b7a1f8dc9b22aafabc2e301
Joel Tannas <jtannas@gmail.com>
2019-02-25 11:04:34 -0700

    get editor stuff set up

--- spec/one_spec.rb
+++ spec/one_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'one'
 
-RSpec.describe DayOne do
+RSpec.describe(DayOne) do
   describe 'the class' do
     subject { DayOne }
 
-    it { is_expected.to be_a(Class) }
+    it { is_expected.to(be_a(Class)) }
   end
 end
