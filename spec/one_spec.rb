d1cc36446029249718a9f88a434c5a1f0d75215d
Joel Tannas <jtannas@gmail.com>
2019-02-25 11:07:56 -0700

    simplify for first time people

--- spec/one_spec.rb
+++ spec/one_spec.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-require 'one'
+require 'spec_helper' # Where the baseline requires and settings to get RSpec running are set
+require 'one' # newer versions of RSpec automatically add lib to the load paths
 
 RSpec.describe(DayOne) do
-  describe 'the class' do
-    subject { DayOne }
-
-    it { is_expected.to(be_a(Class)) }
+  it 'is a class' do
+    expect(DayOne).to be_a(Class)
   end
 end
